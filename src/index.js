8001f902f3539a87a4357e527d1452246626de00
eylee8714 <eylee8714@gmail.com>
2021-05-14 20:55:32 +0900

    Store and Reducer

--- src/index.js
+++ src/index.js
@@ -1,23 +1,16 @@
+import { createStore } from 'redux';
+
 const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
-let count = 0;
-
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
-};
-const handleAdd = () => {
-  count = count + 1;
-  updateText();
+//여기서는 countModifier가 reducer 이다.
+//count를 increase 하거나 decrease한다.
+//return 하는 것이 data가 될것이다.
+const countModifier = (state = 0) => {
+  return state;
 };
 
-const handleMinus = () => {
-  count = count - 1;
-  updateText();
-};
+const countStore = createStore(countModifier);
 
-add.addEventListener('click', handleAdd);
-minus.addEventListener('click', handleMinus);
+console.log(countStore.getState());
