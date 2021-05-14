5075167ece29d608a23d6d78bf31e9040bf74bc3
eylee8714 <eylee8714@gmail.com>
2021-05-14 21:11:44 +0900

    Actions

--- src/index.js
+++ src/index.js
@@ -4,13 +4,28 @@ const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
-//여기서는 countModifier가 reducer 이다.
-//count를 increase 하거나 decrease한다.
-//return 하는 것이 data가 될것이다.
-const countModifier = (state = 0) => {
-  return state;
+// action은 redux의 두번째 인자이다.
+
+const countModifier = (count = 0, action) => {
+  console.log(count, action);
+
+  if (action.type === 'ADD') {
+    return count + 1;
+  } else if (action.type === 'MINUS') {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
 const countStore = createStore(countModifier);
+
+// dispatch 를 하면 reducer 에 액션(메세지)를 보낼 수 있다.
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'MINUS' });
 
 console.log(countStore.getState());
